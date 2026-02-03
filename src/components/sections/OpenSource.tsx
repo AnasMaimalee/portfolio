b4777561e00dfc6cc0cbc9f3f8641dacfe1a7a17
AnasMaimalee <anasment6@gmail.com>
2026-02-04 00:49:18 +0530

    feat: Open Source

--- src/components/sections/OpenSource.tsx
+++ src/components/sections/OpenSource.tsx
@@ -63,7 +63,7 @@ export default function OpenSource() {
                   {pkg.status}
                 </span>
 
-                <h3 className="text-2xl font-bold mb-4">
+                <h3 className="text-xl font-bold mb-4">
                   {pkg.name}
                 </h3>
 
