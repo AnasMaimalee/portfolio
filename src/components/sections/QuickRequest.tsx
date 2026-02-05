31e0f87f431a07a2c53d74284057f6d2af2b0dfe
AnasMaimalee <anasment6@gmail.com>
2026-02-05 19:47:24 +0530

    fix: feedback new backend url

--- src/components/sections/QuickRequest.tsx
+++ src/components/sections/QuickRequest.tsx
@@ -24,7 +24,7 @@ export default function QuickRequest() {
     setShowError(false);
 
     try {
-      const response = await fetch("https://appears-desired-elect-capacity.trycloudflare.com/api/feedback", {
+      const response = await fetch("https://competitions-triple-organization-pride.trycloudflare.com/api/feedback", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
