b1cfdd6e09c408e6efa2b9aef2701d11c3039776
AnasMaimalee <anasment6@gmail.com>
2026-02-18 06:02:16 +0530

    fix: qucik request added new backend url

--- src/components/sections/QuickRequest.tsx
+++ src/components/sections/QuickRequest.tsx
@@ -24,7 +24,7 @@ export default function QuickRequest() {
     setShowError(false);
 
     try {
-      const response = await fetch("https://competitions-triple-organization-pride.trycloudflare.com/api/feedback", {
+      const response = await fetch("https://cite-consultation-naples-dependent.trycloudflare.com/api/feedback", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
